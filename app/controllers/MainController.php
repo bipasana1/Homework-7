571b8cb2e163844b540b69bdbf37d45fbaefb298
bipasana1 <bipasana12@gmail.com>
2024-03-10 18:15:40 -0400

    new

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -17,11 +17,30 @@ class MainController extends Controller
         echo $template->render($homepageData);
     }
     public function notFound() {
+        try {
+            // Load the 404 error template
+            $template = $this->twig->load('404.twig');
+            $errorData = [
+                'title' => '404 Not Found',
+                'message' => 'Sorry, the page you are looking for could not be found.'
+            ];
+    
+            // Render the template
+            echo $template->render($errorData);
+        } catch (\Exception $e) {
+            // Log the exception
+            error_log('Error rendering 404 template: ' . $e->getMessage());
+            // Return a generic error message
+            echo "An error occurred while processing your request. Please try again later.";
+        }
+    }
+    
+    //public function notFound() 
+    //{
+      //  $template = $this->twig->load('notFound/notFound.twig');
+     //   http_response_code(404);
+       // echo $template->render(); 
 
-        $template = $this->twig->load('notFound/notFound.twig');
-        http_response_code(404);
-        echo $template->render(); 
-
-    }   
+   // }   
     
 }
