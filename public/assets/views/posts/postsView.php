2b8c9619cf1c85dfc1437b79e2f3ca65a8ec862a
bipasana1 <bipasana12@gmail.com>
2024-03-17 11:24:16 -0400

    new

--- public/assets/views/posts/postsView.php
+++ public/assets/views/posts/postsView.php
@@ -31,10 +31,6 @@
             <br>
             <input type="text" id="username-input" name="username">
             <br><br>
-            <label for="Title">Post Title:</label>
-            <br>
-            <input  type="text" id="title-input" name="title">
-            <br><br>
             <label for="Description">Description:</label>
             <br>
             <textarea name="description" id="description-input" placeholder="Write something to post..."></textarea>
